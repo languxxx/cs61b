758a9f551d8cadf13c54e1299323b8a98c63dc1e
Zixuan Wang <languxxx@gmail.com>
2020-05-21 14:01:27 -0400

    Finish LeapYear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,11 @@ public class LeapYear {
             }
         }
     }
-}
 
+    public static boolean isLeapYear(int year) {
+      if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        return true;
+      }
+      else return false;
+    }
+}
