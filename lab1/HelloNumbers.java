210a7f1bf57f47f8b366b9ac05cfd723d39bee59
Zixuan Wang <languxxx@gmail.com>
2020-05-21 12:20:20 -0400

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,10 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 0;i <= 9;i++){
+          num += i;
+          System.out.print(num + " ");
+        }
+        System.out.print("\n");
+    }
+}
